c379e003ac6dafd08a6ecc58e44648a89c3d98b3
mlbiam <marc@tremolo.io>
2019-03-07 10:47:54 -0500

    added openunison secret generation

---
+++ src/main/js/globals.js
@@ -0,0 +1,7 @@
+//Global Vars
+var inProp = {};
+var CertUtils = Java.type("com.tremolosecurity.kubernetes.artifacts.util.CertUtils");
+var cfg_ob = {};
+var k8s_obj = {};
+var ouKS;
+var ksPassword;
